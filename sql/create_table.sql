73b9a449decf3171d261642460015f2b215e3102
JXY <wy163jxy@163.com>
2025-10-13 08:59:44 +0800

    fix: userRole修复，删除老师角色

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -24,7 +24,7 @@ CREATE TABLE cms.user
     phone         VARCHAR(256)                       NULL COMMENT '电话',
     email         VARCHAR(512)                       NULL COMMENT '邮箱',
     tags          VARCHAR(1024)                      NULL COMMENT '标签列表',
-    userRole      TINYINT  DEFAULT 0                 NULL COMMENT '用户角色 0-普通用户，1-教师，2-管理员',
+    userRole      TINYINT  DEFAULT 0                 NULL COMMENT '用户角色 0-普通用户，1-管理员',
     createTime    DATETIME DEFAULT CURRENT_TIMESTAMP NULL COMMENT '创建时间',
     updateTime    DATETIME DEFAULT CURRENT_TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
     isDelete      TINYINT  DEFAULT 0                 NOT NULL COMMENT '是否删除',
